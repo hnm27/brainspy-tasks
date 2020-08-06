2b7daba87087b9cb0a0b418e8b16b4f07268ff56
ualegre <u.alegre@utwente.nl>
2020-08-06 09:25:54 +0200

    Renamed performance

--- bspytasks/tasks/ring/classifier.py
+++ bspytasks/tasks/ring/classifier.py
@@ -35,10 +35,10 @@ def ring_task(dataloaders, custom_model, configs, transforms=None, logger=None, 
 
     if len(dataloaders[0]) > 0:
         results['train_results'] = postprocess(dataloaders[0].dataset[dataloaders[0].sampler.indices], model, criterion, logger, main_dir)
-        results['train_results']['performance'] = performances[0]
+        results['train_results']['performance_history'] = performances[0]
     if len(dataloaders[1]) > 0:
         results['dev_results'] = postprocess(dataloaders[1].dataset[dataloaders[1].sampler.indices], model, criterion, logger, main_dir)
-        results['dev_results']['performance'] = performances[1]
+        results['dev_results']['performance_history'] = performances[1]
     if len(dataloaders[2]) > 0:
         results['test_results'] = postprocess(dataloaders[2].dataset[dataloaders[2].sampler.indices], model, criterion, logger, main_dir)
     plot_results(results, plots_dir=results_dir)
@@ -54,8 +54,8 @@ def get_ring_data(gap, configs, transforms, data_dir=None):
         dataset = RingDatasetLoader(data_dir, transforms=transforms, save_dir=data_dir)
     else:
         dataset = RingDatasetGenerator(configs['data']['sample_no'], gap, transforms=transforms, save_dir=data_dir)
-    dataloaders, split_indices = split(dataset, configs['algorithm']['batch_size'], num_workers=configs['algorithm']['worker_no'], split_percentages=configs['data']['split_percentages'])
-    return dataset, dataloaders, split_indices
+    dataloaders = split(dataset, configs['algorithm']['batch_size'], num_workers=configs['algorithm']['worker_no'], split_percentages=configs['data']['split_percentages'])
+    return dataloaders
 
 
 def postprocess(dataset, model, criterion, logger, save_dir=None):
@@ -66,7 +66,7 @@ def postprocess(dataset, model, criterion, logger, save_dir=None):
         indices = torch.argsort(targets[:, 0], dim=0)
         inputs, targets = inputs[indices], targets[indices]
         predictions = model(inputs)
-        results['best_performance'] = criterion(predictions, targets)
+        results['performance'] = criterion(predictions, targets)
 
     #results['gap'] = dataset.gap
     results['inputs'] = inputs
@@ -110,9 +110,9 @@ def plot_results(results, plots_dir=None, show_plots=False, extension='png'):
         plot_output(results['test_results'], 'Test', plots_dir=plots_dir, extension=extension)
     plt.figure()
     plt.title(f'Learning profile', fontsize=12)
-    plt.plot(results['train_results']['performance'], label='Train')
+    plt.plot(TorchUtils.get_numpy_from_tensor(results['train_results']['performance_history']), label='Train')
     if 'dev_results' in results:
-        plt.plot(results['dev_results']['performance'], label='Dev')
+        plt.plot(TorchUtils.get_numpy_from_tensor(results['dev_results']['performance_history']), label='Dev')
     plt.legend()
     if plots_dir is not None:
         plt.savefig(os.path.join(plots_dir, f"training_profile." + extension))
@@ -137,7 +137,7 @@ def plot_results(results, plots_dir=None, show_plots=False, extension='png'):
 def plot_output(results, label, plots_dir=None, extension='png'):
     plt.figure()
     plt.plot(results['best_output'].detach().cpu())
-    plt.title(f"{label} Output (nA) \n Performance: {results['best_performance']} \n Accuracy: {results['accuracy']['accuracy_value']}", fontsize=12)
+    plt.title(f"{label} Output (nA) \n Performance: {results['performance']} \n Accuracy: {results['accuracy']['accuracy_value']}", fontsize=12)
     if plots_dir is not None:
         plt.savefig(os.path.join(plots_dir, label + "_output." + extension))
 
@@ -179,5 +179,5 @@ if __name__ == '__main__':
     ])
     gap = 0.4
     logger = Logger(f'tmp/output/logs/experiment' + str(d.datetime.now().timestamp()))
-    dataset, dataloaders, split_indices = get_ring_data(gap, configs, transforms)
+    dataloaders = get_ring_data(gap, configs, transforms)
     ring_task(dataloaders, DNPU, configs, logger=logger)
