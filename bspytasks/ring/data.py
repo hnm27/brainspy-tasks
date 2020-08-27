5e9186dc5cb27033168f8c29595822ce61a3d14c
ualegre <u.alegre@utwente.nl>
2020-08-27 11:10:10 +0200

    Major improvement on the ring data generation

--- bspytasks/ring/data.py
+++ bspytasks/ring/data.py
@@ -1,4 +1,9 @@
 
+"""
+Created on Thu Aug 27 2020
+This file contains classes related to the ring data generation and loading. 
+@author: Unai Alegre-Ibarra
+"""
 import os
 import torch
 import numpy as np
@@ -14,8 +19,7 @@ class RingDatasetGenerator(Dataset):
         # The sample_no is related to the data that is going to be generated but it actually gets reduced when filtering the circles
         # TODO: Make the dataset generate the exact number of samples as requested by the user
         self.transforms = transforms
-        self.inputs, targets = self.generate_data(sample_no, gap, verbose=verbose)
-        self.targets = targets[:, np.newaxis]
+        self.inputs, self.targets = self.generate_data(sample_no, gap, verbose=verbose)
         self.gap = gap
         assert len(self.inputs) == len(self.targets), "Targets and inputs must have the same length"
 
@@ -35,79 +39,39 @@ class RingDatasetGenerator(Dataset):
         return sample
 
     def generate_data(self, sample_no, gap, verbose=True):
-        length_factor = 20
-
-        while True:
-            data, labels = self.ring(sample_no=sample_no * length_factor, gap=gap)
-            if (min(len(labels[labels == 0]), len(labels[labels == 1])) * 2) > sample_no:
-                break
-            length_factor = length_factor * 2
-
-        data, labels = self.process_dataset(sample_no, data[labels == 0], data[labels == 1])
+        assert sample_no % 2 == 0, 'Only an even sample number is supported.'
+        indices = self.get_balanced_distribution_indices(sample_no)
+        sample_no = int(sample_no / 2)
+        limit = (1 - gap) / 2
+        class0_points = self.get_class_points(sample_no, 0, limit)
+        class0_targets = np.zeros(sample_no)
+        class1_points = self.get_class_points(sample_no, 1 - limit, 1)
+        class1_targets = np.ones(sample_no)
+        points = np.append(class0_points, class1_points)[:, np.newaxis]
+        targets = np.append(class0_targets, class1_targets)[:, np.newaxis]
 
         if verbose:
-            print(f'There are a total of {len(data[labels == 0]) + len(data[labels == 1])} samples')
+            print(f'There are a total of {len(points)} samples')
             print(f'The input ring dataset has a {gap} gap (In a range from -1 to 1).')
 
-        return data, labels
+        return points[indices], targets[indices]
 
-    def ring(self, sample_no, inner_radius=0.25, gap=0.5, outer_radius=1):
-        '''Generates labelled TorchUtilsdata of a ring with class 1 and the center with class 0'''
-        assert outer_radius <= 1
+    def get_class_points(self, sample_no, in_limit, out_limit):
+        # Create a straight line with the size of the dataset
+        linspace_out = np.linspace(0, 2 * np.pi, sample_no, endpoint=False)
+        linspace_in = np.linspace(0, 2 * np.pi, sample_no, endpoint=False)
 
-        samples = (-1 * outer_radius) + (2 * outer_radius * np.random.rand(sample_no, 2))
-        norm = np.sqrt(np.sum((samples)**2, axis=1))
+        # Create the limits of the area in which the points are generated
+        outer_circ_x = np.cos(linspace_out) * out_limit
+        outer_circ_y = np.sin(linspace_out) * out_limit
+        inner_circ_x = np.cos(linspace_in) * in_limit
+        inner_circ_y = np.sin(linspace_in) * in_limit
 
-        # Filter out samples outside the classes
-        labels = np.empty(samples.shape[0])
-        labels[norm < inner_radius] = 0
-        labels[(norm < outer_radius) * (norm > inner_radius + gap)] = 1
-        labels[norm > outer_radius] = np.nan
-        labels[(norm > inner_radius) * (norm < inner_radius + gap)] = np.nan
+        # Get uniformly distributed samples within the limits
+        x = np.random.uniform(low=inner_circ_x, high=outer_circ_x, size=(sample_no,))
+        y = np.random.uniform(low=inner_circ_y, high=outer_circ_y, size=(sample_no,))
 
-        return samples, labels
-
-    def subsample(self, class0, class1):
-        # Subsample the largest class
-        nr_samples = min(len(class0), len(class1))
-        max_array = max(len(class0), len(class1))
-        indices = np.random.permutation(max_array)[:nr_samples]
-        if len(class0) == max_array:
-            class0 = class0[indices]
-        else:
-            class1 = class1[indices]
-        return class0, class1
-
-    def sort(self, class0, class1):
-        # Sort samples within each class wrt the values of input x (i.e. index 0)
-        sorted_index0 = np.argsort(class0, axis=0)[:, 0]
-        sorted_index1 = np.argsort(class1, axis=0)[:, 0]
-        return class0[sorted_index0], class1[sorted_index1]
-
-    def filter_and_reverse(self, class0, class1):
-        # Filter by positive and negative values of y-axis
-        class0_positive_y = class0[class0[:, 1] >= 0]
-        class0_negative_y = class0[class0[:, 1] < 0][::-1]
-        class1_positive_y = class1[class1[:, 1] >= 0]
-        class1_negative_y = class1[class1[:, 1] < 0][::-1]
-
-        # Define input variables and their target
-        class0 = np.concatenate((class0_positive_y, class0_negative_y))
-        class1 = np.concatenate((class1_positive_y, class1_negative_y))
-        inputs = np.concatenate((class0, class1))
-        targets = np.concatenate((np.zeros_like(class0[:, 0]), np.ones_like(class1[:, 0])))
-
-        # Reverse negative 'y' inputs for negative cases
-        return inputs, targets
-
-    def process_dataset(self, sample_no, class0, class1):
-        class0, class1 = self.subsample(class0, class1)
-        class0, class1 = self.reduce_length(sample_no, class0, class1)
-        class0, class1 = self.sort(class0, class1)
-        inputs, targets = self.filter_and_reverse(class0, class1)
-        indices = self.get_balanced_distribution_indices(len(targets))
-
-        return inputs[indices], targets[indices]
+        return np.vstack((x, y)).T
 
     def get_balanced_distribution_indices(self, data_length):
         permuted_indices = np.random.permutation(data_length)
@@ -119,17 +83,6 @@ class RingDatasetGenerator(Dataset):
             result.append(class0[i])
             result.append(class1[i])
         return np.array(result)
-
-    def reduce_length(self, sample_no, class0, class1):
-        max_index = int(sample_no / 2)
-        return class0[:max_index], class1[:max_index]
-
-    def transform_gap(self, gap_in_volts, scale):
-        assert (len(scale[scale == scale.mean()]) == len(scale)), "The GAP information is going to be inaccurate because the selected input electrodes have a different voltage range. In order for this data to be accurate, please make sure that the input electrodes have the same voltage ranges."
-        if len(scale) > 1:
-            scale = scale[0]
-
-        return (gap_in_volts / scale)
 
 
 class RingDatasetLoader(Dataset):
